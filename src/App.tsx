589616f02626cb358e9aff34c0b57ba9c9743629
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-12 07:40:40 +0530

    changed bg color

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./pageComponent/Navbar"
 
 const App = () => {
   return (
-    <div className="w-full h-screen text-white bg-slate-950">
+    <div className="w-full h-screen text-white bg-[#2E073F]">
       <Navbar/>
     </div>
   )
