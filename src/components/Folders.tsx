67b06b8afb7b0ca4a3bff20746d1237b31c33509
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-30 23:22:31 +0530

    explore page for mobile view done

--- src/components/Folders.tsx
+++ src/components/Folders.tsx
@@ -3,63 +3,98 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFolder, faFolderOpen } from '@fortawesome/free-solid-svg-icons';
 import FoldersComp from './FoldersComp';
 
-const folderData = [
+// Define the type for a folder item
+interface Folder {
+    year: number;
+    context: string;
+    image: string;
+    achievements: string[];
+}
+
+// Define the folder data with the Folder type
+const folderData: Folder[] = [
     {
         year: 2022,
         context: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dicta architecto necessitatibus.',
         image: 'https://www.webtekno.com/images/editor/default/0003/45/aeb949ba1c92aa0a55bd438657bb2de0d9708db6.jpeg',
+        achievements: [
+            "400+ participants",
+            "50+ projects submitted",
+            "$5,000+ prize pool",
+            "10+ sponsors, 100+ workshop attendees",
+            "10 Workshops",
+            "Industry professional judges",
+        ],
     },
     {
         year: 2023,
         context: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dicta architecto necessitatibus.',
         image: 'https://i0.wp.com/tecknoworks.com/wp-content/uploads/2020/01/hackathon-1.png',
+        achievements: [
+            "400+ participants",
+            "50+ projects submitted",
+            "$5,000+ prize pool",
+            "10+ sponsors, 100+ workshop attendees",
+            "10 Workshops",
+            "Industry professional judges",
+        ],
     },
     {
         year: 2024,
         context: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dicta architecto necessitatibus.',
         image: 'https://ohze.co/content/images/size/w2000/2018/11/Hackathon-Coolblue-2017-def.jpg',
+        achievements: [
+            "400+ participants",
+            "50+ projects submitted",
+            "$5,000+ prize pool",
+            "10+ sponsors, 100+ workshop attendees",
+            "10 Workshops",
+            "Industry professional judges",
+        ],
     },
 ];
 
-const Folders = () => {
-    const [openYear, setOpenYear] = useState(2024); 
-    return (
-    <div className="flex flex-col justify-center items-center lg:flex-row lg:justify-between lg:gap-40 lg:mx-20">
-        {/* Folder Selector */}
-        <div className="mt-4">
-        <div className="flex flex-wrap justify-center items-center gap-5 lg:gap-10 lg:justify-start">
-            {folderData.map((folder) => (
-            <div
-                key={folder.year}
-                className="flex flex-col items-center cursor-pointer"
-                onClick={() => setOpenYear(folder.year)}
-            >
-                <FontAwesomeIcon
-                icon={openYear === folder.year ? faFolderOpen : faFolder}
-                className="text-4xl lg:text-5xl text-purple-500 transition-transform transform hover:scale-110"
-                />
-                <p className="text-sm lg:text-base">{folder.year}</p>
-            </div>
-            ))}
-        </div>
-    </div>
+const Folders: React.FC = () => {
+    const [openYear, setOpenYear] = useState<number>(2024); 
 
-      {/* Folder Content */}
-        <div className="mt-6 w-full max-w-4xl lg:h-40">
-        {folderData.map(
-            (folder) =>
-            openYear === folder.year && (
-                <FoldersComp
-                key={folder.year}
-                year={folder.year}
-                context={folder.context}
-                image={folder.image}
-                />
-            )
-        )}
+    return (
+        <div className="flex flex-col justify-center items-center lg:flex-row lg:justify-between lg:gap-40 lg:mx-20">
+            {/* Folder Selector */}
+            <div className="mt-4">
+                <div className="flex flex-wrap justify-center items-center gap-5 lg:gap-10 lg:justify-start">
+                    {folderData.map((folder) => (
+                        <div
+                            key={folder.year}
+                            className="flex flex-col items-center cursor-pointer"
+                            onClick={() => setOpenYear(folder.year)}
+                        >
+                            <FontAwesomeIcon
+                                icon={openYear === folder.year ? faFolderOpen : faFolder}
+                                className="text-4xl lg:text-5xl text-purple-500 transition-transform transform hover:scale-110"
+                            />
+                            <p className="text-sm lg:text-base">{folder.year}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* Folder Content */}
+            <div className="mt-6 w-full max-w-4xl lg:h-40">
+                {folderData.map(
+                    (folder) =>
+                        openYear === folder.year && (
+                            <FoldersComp
+                                key={folder.year}
+                                year={folder.year}
+                                context={folder.context}
+                                image={folder.image}
+                                achievements={folder.achievements}
+                            />
+                        )
+                )}
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default Folders;
--- src/components/FoldersComp.tsx
+++ src/components/FoldersComp.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
-interface FolderCompProps{
-    year:number;
+interface FolderCompProps {
+    year: number;
     context: string;
     image: string;
-}
-const FoldersComp: React.FC<FolderCompProps> = ({year, context, image}) => {
-    return (
-        <div className='flex flex-col justify-between items-center p-2'>
-            <div className='flex flex-wrap gap-1 border-2 p-2 border-pink-900 rounded-lg'>
-                <div className='p-2 rounded-lg'>
-                    <h1 className='text-lg font-bold text-pink-500 lg:text-2xl'>{year}</h1>
-                    <p className='text-sm'>{context}</p>
-                </div>
-                <div className='border-2 border-pink-900 p-2 rounded-lg'>
-                    <img src={image} alt="img" className='rounded' />
-                </div>
-            </div>
-        </div>
-    )
+    achievements: string[];
 }
 
-export default FoldersComp
+const FoldersComp: React.FC<FolderCompProps> = ({ year, context, image, achievements }) => {
+    return (
+        <div className="flex flex-col justify-between items-start p-2 lg:flex-row lg:mt-10">
+            {/* Achievements Section */}
+            
+
+            {/* Year and Context Section */}
+            <div className="flex flex-wrap justify-center gap-1 border-2 border-pink-900 lg:w-2/3  rounded-lg">
+                <h1 className="text-2xl font-bold text-pink-500 lg:text-2xl">{year}</h1>
+                <div className="border-2 border-pink-900 p-2 rounded-lg">
+                    <img src={image} alt="img" className="rounded h-44" />
+                </div>
+                <div className="p-2 rounded-lg">
+                    <p className="text-sm">{context}</p>
+                    <div className="mt-4 lg:w-1/3 lg:mt-0 lg:ml-4">
+                <h1 className="text-lg font-bold text-pink-500">Achievements</h1>
+                <ul className="pl-5 text-sm text-gray-300">
+                    
+                    {achievements.map((achievement, index) => (
+                        <li key={index}><FontAwesomeIcon
+                        icon={faCircleCheck}
+                        className="mr-2 text-purple-500 transition-transform transform hover:scale-110"
+                    />
+                    {achievement}</li>
+                    ))}
+                </ul>
+            </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default FoldersComp;
