fb522eda417d91bd13281ed30883fbb4e12bedef
ankitsensei <main.ankitbhagat@gmail.com>
2024-12-04 22:35:26 +0530

    added FOOTER

--- src/pageComponent/Footer.tsx
+++ src/pageComponent/Footer.tsx
@@ -1,86 +1,75 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiscord, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
-import Image from "next/image";
 
 const Footer: React.FC = () => {
     return (
-        <div className="items-center text-white">
-            <footer className="mt-5 md:mx-auto md:flex p-8 md:justify-between w-full md:w-[80%]">
-                <div className="w-full md:p-4 py-6 lg:py-2">
-                    <div className="md:flex md:justify-between">
-                        <div className="mb-6 justify-center md:mb-0">
-                            <div className="relative">
-                                <Image
-                                    alt="Hack United"
-                                    width={110}
-                                    height={110}
-                                    src="./assets/Logos/HackUnited3.png"
-                                    className="absolute mt-[-5px]"
-                                />
-                                <h1 className="text-3xl ml-[95px] md:ml-[80px] md:text-center text-[#DF6A50] hackunited text-savapro">
-                                    HACK
-                                </h1>
-                                <h1 className="text-xl ml-[100px] md:ml-[80px] md:text-center text-[#DF6A50] hackunited text-savapro">
-                                    UNITED
-                                </h1>
-                            </div>
-                            <h3 className="text-gray-600 text-[0.9rem] mt-5 dark:text-gray-400 font-medium">
-                                EIN: 81-2908499
-                            </h3>
-                            <div className="pt-[1vh]">
-                                <div className="flex flex-row space-x-4">
-                                    <Link
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href="https://www.linkedin.com/company/hack-united"
-                                    >
-                                        <FontAwesomeIcon
-                                            width={30}
-                                            color="white"
-                                            icon={faLinkedin}
-                                        />
-                                    </Link>
-                                    <Link
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href="https://instagram.com/hack_united"
-                                    >
-                                        <FontAwesomeIcon
-                                            width={30}
-                                            color="white"
-                                            icon={faInstagram}
-                                        />
-                                    </Link>
-                                    <Link
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href="https://twitter.com/hackunited_"
-                                    >
-                                        <FontAwesomeIcon
-                                            width={35}
-                                            color="white"
-                                            icon={faTwitter}
-                                        />
-                                    </Link>
-                                    <Link
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href="https://discord.gg/hackunited"
-                                    >
-                                        <FontAwesomeIcon
-                                            width={45}
-                                            color="white"
-                                            icon={faDiscord}
-                                        />
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <footer className="bg-gradient-to-r from-purple-600 via-purple-800 to-purple-900 text-white py-8">
+            <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
+                {/* Logo and Tagline */}
+                <div className="mb-6 md:mb-0">
+                    <h1 className="text-2xl font-bold">HACK UNITED</h1>
+                    <p className="text-sm text-gray-300 mt-2">Innovating Together for a Better Tomorrow</p>
                 </div>
-            </footer>
-        </div>
+
+                {/* Navigation Links */}
+                <div className="flex space-x-6 text-sm">
+                    <Link href="#home" className="hover:text-gray-300">
+                        Home
+                    </Link>
+                    <Link href="#about" className="hover:text-gray-300">
+                        About
+                    </Link>
+                    <Link href="#sponsors" className="hover:text-gray-300">
+                        Sponsors
+                    </Link>
+                    <Link href="#faq" className="hover:text-gray-300">
+                        FAQ
+                    </Link>
+                </div>
+
+                {/* Social Links */}
+                <div className="flex space-x-4 mt-6 md:mt-0">
+                    <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.linkedin.com/company/hack-united"
+                        className="hover:text-gray-300"
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} size="lg" />
+                    </Link>
+                    <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://instagram.com/hack_united"
+                        className="hover:text-gray-300"
+                    >
+                        <FontAwesomeIcon icon={faInstagram} size="lg" />
+                    </Link>
+                    <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://twitter.com/hackunited_"
+                        className="hover:text-gray-300"
+                    >
+                        <FontAwesomeIcon icon={faTwitter} size="lg" />
+                    </Link>
+                    <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://discord.gg/hackunited"
+                        className="hover:text-gray-300"
+                    >
+                        <FontAwesomeIcon icon={faDiscord} size="lg" />
+                    </Link>
+                </div>
+            </div>
+
+            {/* Bottom Bar */}
+            <div className="text-center text-gray-400 text-sm mt-6">
+                © {new Date().getFullYear()} Hack United. All rights reserved.
+            </div>
+        </footer>
     );
 };
 
