e542aac1f82535811c69cd493fa5b44dc583e6be
Ankit Bhagat <main.ankitbhagat@gmail.com>
2025-01-17 19:37:38 +0530

    imported all sponsors images in sponsors.tsx file

--- src/pageComponent/Sponsors.tsx
+++ src/pageComponent/Sponsors.tsx
@@ -1,4 +1,13 @@
-
+import CodeCrafter from "../assets/Sponsors/CodeCrafters.io_full_logo_Dark_text.webp"
+import Aops from "../assets/Sponsors/aopsLogo.webp"
+import Balsamidq from "../assets/Sponsors/balsamiqLogo.webp"
+import Cocalc from "../assets/Sponsors/cocalcLogo.webp"
+import Desmos from "../assets/Sponsors/desmosLogo.webp"
+import Devitjobs from "../assets/Sponsors/devitjobsLogo.webp"
+import Genxyz from "../assets/Sponsors/genxyzLogo.webp"
+import InterviewCake from "../assets/Sponsors/interviewcakeLogo.webp"
+import Tintype from "../assets/Sponsors/tintypeLogo.webp"
+import TreeHouses from "../assets/Sponsors/Treehouse_logo.webp"
 const Sponsors = () => {
     return (
         <div className="flex flex-col items-center mx-4">
@@ -9,7 +18,7 @@ const Sponsors = () => {
             <div className="mt-10 flex flex-col gap-5 justify-center items-center">
                 <div id="main" className="flex flex-col gap-2"> 
                     <a href="#" className="rounded-lg">
-                        <img src="https://www.goomlandscapes.co.nz/wp-content/uploads/2018/08/logo-placeholder.png" alt="Placeholder Image" className="rounded lg:h-64" />
+                        <img src={CodeCrafter} alt="Placeholder Image" className="rounded lg:h-64" />
                     </a>
                     <a href="#">
                         <img src="https://www.goomlandscapes.co.nz/wp-content/uploads/2018/08/logo-placeholder.png" alt="Cover Image" className="rounded lg:h-64"/>
